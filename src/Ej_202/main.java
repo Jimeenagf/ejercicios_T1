75eaf1d1bb38412b946dac69e7484ae06fbfa4af
Jimena <jimena.gil@alumnos.upm.es>
2026-09-21 10:27:16 +0200

    segundo ejercicio hecho

---
+++ src/Ej_202/main.java
@@ -0,0 +1,10 @@
+package Ej_202;
+
+public class main {
+
+		public static void main(String[] args) {
+			System.out.println("Máximo valor de char: " + Character.MAX_VALUE);
+			System.out.println("Máximo valor de short: " + Short.MAX_VALUE);
+			System.out.println("Máximo valor de int: " + Integer.MAX_VALUE);
+		}
+	}
